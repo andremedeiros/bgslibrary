02495410e9a2216976f1c9bf8a1adf1807f19eec
andrewssobral@gmail.com <andrewssobral@gmail.com@7e9cb7ec-d131-0c6a-8031-cf146dc18c44>
2013-01-21 23:21:58 +0000

    Some adjustments and two new algorithms.
    git-svn-id: http://bgslibrary.googlecode.com/svn/trunk@23 7e9cb7ec-d131-0c6a-8031-cf146dc18c44

--- Demo.cpp
+++ Demo.cpp
@@ -20,10 +20,14 @@
 
 #include "package_bgs/tb/T2FGMM_UM.h"
 #include "package_bgs/tb/T2FGMM_UV.h"
+#include "package_bgs/tb/T2FMRF_UM.h"
+#include "package_bgs/tb/T2FMRF_UV.h"
 #include "package_bgs/tb/FuzzySugenoIntegral.h"
 #include "package_bgs/tb/FuzzyChoquetIntegral.h"
 
 #include "package_bgs/jmo/MultiLayerBGS.h"
+
+#include "package_bgs/pt/PixelBasedAdaptiveSegmenter.h"
 
 #include "package_bgs/lb/LBSimpleGaussian.h"
 #include "package_bgs/lb/LBFuzzyGaussian.h"
@@ -34,61 +38,67 @@
 int main(int argc, char **argv)
 {
   CvCapture *capture = 0;
-  
-  capture = cvCaptureFromCAM(0);
-  //capture = cvCaptureFromAVI("video.avi");
-  
+  capture = cvCaptureFromCAM(0); //capture = cvCaptureFromAVI("video.avi");
   if(!capture){
-    std::cerr << "Cannot open initialize webcam!" << std::endl;
+    std::cerr << "Cannot initialize webcam!" << std::endl;
     return 1;
   }
   
-  IplImage *frame = cvQueryFrame(capture);
+  int resize_factor = 50; // 50% of original image
+  IplImage *frame_aux = cvQueryFrame(capture);
+  IplImage *frame = cvCreateImage(cvSize((int)((frame_aux->width*resize_factor)/100) , (int)((frame_aux->height*resize_factor)/100)), frame_aux->depth, frame_aux->nChannels);
   
   /* Background Subtraction Methods */
-  
-  FrameDifferenceBGS* bgs = new FrameDifferenceBGS;
-  //StaticFrameDifferenceBGS* bgs = new StaticFrameDifferenceBGS;
-  //WeightedMovingMeanBGS* bgs = new WeightedMovingMeanBGS;
-  //WeightedMovingVarianceBGS* bgs = new WeightedMovingVarianceBGS;
-  //MixtureOfGaussianV1BGS* bgs = new MixtureOfGaussianV1BGS;
-  //MixtureOfGaussianV2BGS* bgs = new MixtureOfGaussianV2BGS;
-  //AdaptiveBackgroundLearning* bgs = new AdaptiveBackgroundLearning;
+  IBGS *bgs;
+
+  /*** Default Package ***/
+  bgs = new FrameDifferenceBGS;
+  //bgs = new StaticFrameDifferenceBGS;
+  //bgs = new WeightedMovingMeanBGS;
+  //bgs = new WeightedMovingVarianceBGS;
+  //bgs = new MixtureOfGaussianV1BGS;
+  //bgs = new MixtureOfGaussianV2BGS;
+  //bgs = new AdaptiveBackgroundLearning;
   
   /*** DP Package (adapted from Donovan Parks) ***/
-  //DPAdaptiveMedianBGS* bgs = new DPAdaptiveMedianBGS;
-  //DPGrimsonGMMBGS* bgs = new DPGrimsonGMMBGS;
-  //DPZivkovicAGMMBGS* bgs = new DPZivkovicAGMMBGS;
-  //DPMeanBGS* bgs = new DPMeanBGS;
-  //DPWrenGABGS* bgs = new DPWrenGABGS;
-  //DPPratiMediodBGS* bgs = new DPPratiMediodBGS;
-  //DPEigenbackgroundBGS* bgs = new DPEigenbackgroundBGS;
+  //bgs = new DPAdaptiveMedianBGS;
+  //bgs = new DPGrimsonGMMBGS;
+  //bgs = new DPZivkovicAGMMBGS;
+  //bgs = new DPMeanBGS;
+  //bgs = new DPWrenGABGS;
+  //bgs = new DPPratiMediodBGS;
+  //bgs = new DPEigenbackgroundBGS;
 
   /*** TB Package (adapted from Thierry Bouwmans) ***/
-  //T2FGMM_UM* bgs = new T2FGMM_UM;
-  //T2FGMM_UV* bgs = new T2FGMM_UV;
-  //FuzzySugenoIntegral* bgs = new FuzzySugenoIntegral;
-  //FuzzyChoquetIntegral* bgs = new FuzzyChoquetIntegral;
+  //bgs = new T2FGMM_UM;
+  //bgs = new T2FGMM_UV;
+  //bgs = new T2FMRF_UM;
+  //bgs = new T2FMRF_UV;
+  //bgs = new FuzzySugenoIntegral;
+  //bgs = new FuzzyChoquetIntegral;
 
   /*** JMO Package (adapted from Jean-Marc Odobez) ***/
-  //MultiLayerBGS* bgs = new MultiLayerBGS;
+  //bgs = new MultiLayerBGS;
+
+  /*** PT Package (adapted from Hofmann) ***/
+  //bgs = new PixelBasedAdaptiveSegmenter;
 
   /*** LB Package (adapted from Laurence Bender) ***/
-  //LBSimpleGaussian* bgs = new LBSimpleGaussian;
-  //LBFuzzyGaussian* bgs = new LBFuzzyGaussian;
-  //LBMixtureOfGaussians* bgs = new LBMixtureOfGaussians;
-  //LBAdaptiveSOM* bgs = new LBAdaptiveSOM;
-  //LBFuzzyAdaptiveSOM* bgs = new LBFuzzyAdaptiveSOM;
+  //bgs = new LBSimpleGaussian;
+  //bgs = new LBFuzzyGaussian;
+  //bgs = new LBMixtureOfGaussians;
+  //bgs = new LBAdaptiveSOM;
+  //bgs = new LBFuzzyAdaptiveSOM;
 
   int key = 0;
   while(key != 'q')
   {
-    frame = cvQueryFrame(capture);
+    frame_aux = cvQueryFrame(capture);
+    if(!frame_aux) break;
 
-    if(!frame) break;
+    cvResize(frame_aux, frame);
 
-    cv::Mat img_input(frame,true);
-    cv::GaussianBlur(img_input, img_input, cv::Size(7,7), 1.5);
+    cv::Mat img_input(frame);
     cv::imshow("input", img_input);
 
     cv::Mat img_gray;
@@ -98,7 +108,7 @@ int main(int argc, char **argv)
     // bgs internally shows the foreground mask image
     cv::Mat img_mask;
     bgs->process(img_gray, img_mask); // default
-    //bgs->process(img_input, img_mask); // use it for JMO Package and LB Package
+    //bgs->process(img_input, img_mask); // use it for JMO, PT, TB and LB pkg's
     
     //if(!img_mask.empty())
     //  do something
