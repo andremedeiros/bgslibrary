600e46d44867da882dd47bef85845b72b83b1e27
andrewssobral@gmail.com <andrewssobral@gmail.com@7e9cb7ec-d131-0c6a-8031-cf146dc18c44>
2013-01-23 07:03:04 +0000

    Some adjustments.
    git-svn-id: http://bgslibrary.googlecode.com/svn/trunk@29 7e9cb7ec-d131-0c6a-8031-cf146dc18c44

--- package_bgs/pt/PixelBasedAdaptiveSegmenter.cpp
+++ package_bgs/pt/PixelBasedAdaptiveSegmenter.cpp
@@ -1,6 +1,8 @@
 #include "PixelBasedAdaptiveSegmenter.h"
 
-PixelBasedAdaptiveSegmenter::PixelBasedAdaptiveSegmenter() : firstTime(true), showOutput(true), enableInputBlur(true), enableOutputBlur(true)
+PixelBasedAdaptiveSegmenter::PixelBasedAdaptiveSegmenter() : firstTime(true), showOutput(true), enableInputBlur(true), enableOutputBlur(true),
+alpha(7.0), beta(1.0), N(20), Raute_min(2), R_incdec(0.05), R_lower(18),
+R_scale(5), T_dec(0.05), T_inc(1), T_init(18), T_lower(2), T_upper(200)
 {
   std::cout << "PixelBasedAdaptiveSegmenter()" << std::endl;
 }
@@ -19,18 +21,18 @@ void PixelBasedAdaptiveSegmenter::process(const cv::Mat &img_input, cv::Mat &img
 
   if(firstTime)
   {
-    pbas.setAlpha(7.0);
-    pbas.setBeta(1.0);
-    pbas.setN(20);
-    pbas.setRaute_min(2);
-    pbas.setR_incdec(0.05);
-    pbas.setR_lower(18);
-    pbas.setR_scale(5);
-    pbas.setT_dec(0.05);
-    pbas.setT_inc(1);
-    pbas.setT_init(18);
-    pbas.setT_lower(2);
-    pbas.setT_upper(200);
+    pbas.setAlpha(alpha);
+    pbas.setBeta(beta);
+    pbas.setN(N);
+    pbas.setRaute_min(Raute_min);
+    pbas.setR_incdec(R_incdec);
+    pbas.setR_lower(R_lower);
+    pbas.setR_scale(R_scale);
+    pbas.setT_dec(T_dec);
+    pbas.setT_inc(T_inc);
+    pbas.setT_init(T_init);
+    pbas.setT_lower(T_lower);
+    pbas.setT_upper(T_upper);
 
     saveConfig();
   }
@@ -61,6 +63,19 @@ void PixelBasedAdaptiveSegmenter::saveConfig()
 
   cvWriteInt(fs, "enableInputBlur", enableInputBlur);
   cvWriteInt(fs, "enableOutputBlur", enableOutputBlur);
+  
+  cvWriteReal(fs, "alpha", alpha);
+  cvWriteReal(fs, "beta", beta);
+  cvWriteInt(fs, "N", N);
+  cvWriteInt(fs, "Raute_min", Raute_min);
+  cvWriteReal(fs, "R_incdec", R_incdec);
+  cvWriteInt(fs, "R_lower", R_lower);
+  cvWriteInt(fs, "R_scale", R_scale);
+  cvWriteReal(fs, "T_dec", T_dec);
+  cvWriteInt(fs, "T_inc", T_inc);
+  cvWriteInt(fs, "T_init", T_init);
+  cvWriteInt(fs, "T_lower", T_lower);
+  cvWriteInt(fs, "T_upper", T_upper);
 
   cvWriteInt(fs, "showOutput", showOutput);
 
@@ -74,6 +89,19 @@ void PixelBasedAdaptiveSegmenter::loadConfig()
   enableInputBlur = cvReadIntByName(fs, 0, "enableInputBlur", true);
   enableOutputBlur = cvReadIntByName(fs, 0, "enableOutputBlur", true);
   
+  alpha = cvReadRealByName(fs, 0, "alpha", 7.0);
+  beta = cvReadRealByName(fs, 0, "beta", 1.0);
+  N = cvReadIntByName(fs, 0, "N", 20);
+  Raute_min = cvReadIntByName(fs, 0, "Raute_min", 2);
+  R_incdec = cvReadRealByName(fs, 0, "R_incdec", 0.05);
+  R_lower = cvReadIntByName(fs, 0, "R_lower", 18);
+  R_scale = cvReadIntByName(fs, 0, "R_scale", 5);
+  T_dec = cvReadRealByName(fs, 0, "T_dec", 0.05);
+  T_inc = cvReadIntByName(fs, 0, "T_inc", 1);
+  T_init = cvReadIntByName(fs, 0, "T_init", 18);
+  T_lower = cvReadIntByName(fs, 0, "T_lower", 2);
+  T_upper = cvReadIntByName(fs, 0, "T_upper", 200);
+
   showOutput = cvReadIntByName(fs, 0, "showOutput", true);
 
   cvReleaseFileStorage(&fs);
